5e2eafed75677d9e429fd0482a45b65f67c3d978
ayman003 <aymanadel003@gmail.com>
2018-07-26 16:52:18 +0200

    first release

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleInstrumentedTest.java
@@ -0,0 +1,38 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+
+    @Test
+    public void shouldShowToast() {
+        onView(withText("TELL JOKE")).perform(click());
+        onView(withId(R.id.jokeTextView)).check(matches(withText(not(""))));
+    }
+}
